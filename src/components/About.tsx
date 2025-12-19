2132221df8b3cb095cdac3128d6b4248859f69a1
DawoodBuilds <hm.davidhussain@gmail.com>
2025-12-19 08:06:33 +0500

    Fix: Capitalize About string

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,22 @@
 import "./styles/About.css";
 import { config } from "../config";
 
+function titleCase(str: string): string {
+  const splitStr = str.toLowerCase().split(" ");
+
+  for (let i = 0; i < splitStr.length; i++) {
+    splitStr[i] =
+      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  return splitStr.join(" ");
+}
+
 const About = () => {
   return (
     <div className="about-section" id="about">
       <div className="about-me">
         <h3 className="title">{config.about.title}</h3>
-        <p className="para">
-          {config.about.description}
-        </p>
+        <p className="para">{titleCase(config.about.description)}</p>
       </div>
     </div>
   );
